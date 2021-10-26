138f493fa2a906a367e093cce838f951bc8d8396
EXADEL\omohylivska <mogilevskaya1994@gmail.com>
2021-10-26 15:23:46 +0300

    N5/Add folders

--- front-end/src/views/Home/Home.tsx
+++ front-end/src/views/Home/Home.tsx
@@ -2,9 +2,15 @@ import React, { useEffect } from 'react';
 import { fetchUser } from '../../store/actions';
 import { useDispatch } from 'react-redux';
 import { HomeStepper } from '../../components/HomeStepper';
+import axios from 'axios';
 
 export const Home: React.FC = () => {
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios('/helloworld')
+      .then((res) => console.log(res.data));
+  }, []);
 
   useEffect(() => {
     dispatch(fetchUser());
